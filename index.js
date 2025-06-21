d93ae6ed2be35a20602b3f60b872b5cc84501a1b
Rami Fahil <Fahilinvestments@gmail.com>
2025-06-21 04:29:21 -0700

    Fix duplicate app.listen for Render

--- index.js
+++ index.js
@@ -29,7 +29,3 @@ app.get('/scan', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
-
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-});
